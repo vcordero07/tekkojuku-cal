5b98f3ecf902915367fe002244afa0bcb108bde6
vcordero07 <vcordero07@gmail.com>
2017-09-04 21:18:21 -0400

    wip-mocha chai

---
+++ test/test-instructor.js
@@ -0,0 +1,32 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+// const {
+//   app,
+//   runServer,
+//   closeServer
+// } = require('../app');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('asdfasdfasdf', function() {
+  // before(function() {
+  //   return runServer();
+  // });
+  //
+  // after(function() {
+  //   return closeServer();
+  // });
+
+  it('it should work', function() {
+    // return chai.request(app)
+    //   .get('/instructor/all')
+    //   .then(function(res) {
+    //     res.should.have.status(200);
+    //   });
+    return true
+  });
+
+});
