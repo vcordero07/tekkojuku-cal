6eb22d1d0a97fd45e7df139f5ff32a9ac50527fe
vcordero07 <vcordero07@gmail.com>
2017-09-21 22:46:16 -0400

    wip

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,16 +6,23 @@ let createEventListers = () => {
     //console.log('info:', info);
     $.ajax({
       type: "POST",
-      url: '/auth/login',
+      url: "/auth/login",
       headers: {
         "Authorization": auth,
         "Content-Type": "application/json"
       },
       data: {
-        username: $('#username').val(),
-        password: $('#password').val(),
-        email: $('#email').val()
-      }
+        "username": $('#username').val(),
+        "password": $('#password').val(),
+        "email": $('#email').val()
+      },
+      // success: function(responseData, txtStatus, jqXHR) {
+      //   alert('data saved');
+      // },
+      // error: function(jqXHR, txtStatus, err) {
+      //   console.log(err);
+      // }
+      // datatype: "json"
     })
   });
 }
