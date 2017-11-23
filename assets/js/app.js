79fbab4983d5883fb6454e6caf989f8a1bd4c5a8
vcordero07 <vcordero07@gmail.com>
2017-11-23 13:02:39 -0500

    wip

--- assets/js/app.js
+++ assets/js/app.js
@@ -266,7 +266,7 @@ let createEventListers = () => {
     <br> The purpose of this site is to allow the head instructor to automate the process of the monthly calendar class assignment. You can view, add, edit, and delete classes by going to the Calendar page. To add classes, simply click on "Add Event" and fill the requirements. You can add an Instructor by clicking on the "add Instructor" button, and view his information by clicking on his picture.
     <br> Aikido Tekkojuku Boston (ATJB) was founded to promote the practice and teaching of the Japanese martial art of Aikido, as created by O Sensei Morihei Ueshiba, in an atmosphere of mutual respect conducive to training for all, irrespective of race, ethnicity, gender, ability or age. For more on information, go to <a href="http://aikidotjboston.org/">aikidotjboston.org</a>
 
-    &copy Tekkojuku-Cal by Virgilio Cordero |
+    &copy Tekkojuku-Cal by <a href="https://github.com/vcordero07/tekkojuku-cal">Virgilio Cordero</a> |
       `,
       type: BootstrapDialog.TYPE_PRIMARY,
       cssClass: 'bs-dialog',
